536cfb9b2e22562ec7168e7f9afa124952f44c0f
ohanoch <ohanoch@gmail.com>
2018-08-23 08:34:10 +0200

    added excel_import to common settings file

--- dashboard/settings/common.py
+++ dashboard/settings/common.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # dashboard apps
     'dashboard.apps.dashboard_api',
+	'dashboard.apps.excel_import'
     # external apps
     'rest_framework',
 ]
