422d65434f07dcee44648c5367c8ac0c5be78768
ohanoch <ohanoch@gmail.com>
2018-08-24 13:59:39 +0200

    rearranged to have foreign key ordering in order

--- dashboard/apps/dashboard_api/models.py
+++ dashboard/apps/dashboard_api/models.py
@@ -1,10 +1,17 @@
 from django.db import models
 
+# Table for program (i.e BSc General) info
+class ProgramInfo(models.Model):
+	program_code = models.CharField(max_length=5, primary_key=True)
+	program_title = models.CharField(max_length=255)
+
+	class Meta:
+		verbose_name = "Program information (i.e information about BSc General Program)"
 
 # Table for student personal information
 # Foreign key to program_code in ProgramInfo
 class StudentInfo(models.Model):
-	encrypted_student_no = models.PositiveSmallIntegerField(primary_key=True)
+	encrypted_student_no = models.CharField(max_length=40, primary_key=True)
 	nationality_short_name = models.CharField(max_length=255)
 	home_language_description = models.CharField(max_length=30)
 	race_description = models.CharField(max_length=30)
@@ -17,14 +24,6 @@ class StudentInfo(models.Model):
 	
 	class Meta:
 		verbose_name = "Student personal information"
-
-# Table for program (i.e BSc General) info
-class ProgramInfo(models.Model):
-	program_code = models.CharField(max_length=5, primary_key=True)
-	program_title = models.CharField(max_length=255)
-
-	class Meta:
-		verbose_name = "Program information (i.e information about BSc General Program)"
 
 # Stats of a student in a certain course in a certain year
 # Foreign keys to student_number in StudentInfo
@@ -42,6 +41,8 @@ class CourseStats(models.Model):
 	class Meta:
 		verbose_name = "Information about a student for a course in a specific calendar year"
 
+
+# Average mark for a student in a specific calendar year
 class AverageYearMarks(models.Model):
 	calendar_instance_year = models.CharField(max_length=4)
 	year_of_study = models.PositiveSmallIntegerField	# Refers to YOS student is registered for
@@ -50,4 +51,4 @@ class AverageYearMarks(models.Model):
 	average_marks = models.DecimalField(max_digits=6, decimal_places=3)
 
 	class Meta:
-		verbose_name = "Average mark for a student in a pecific calendar year"
+		verbose_name = "Average mark for a student in a specific calendar year"
