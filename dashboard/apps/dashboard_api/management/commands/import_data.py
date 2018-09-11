855bbc77bbdcba8cca00b4f7f7e6b053b63e0489
Nathan Michlo <1386161@students.wits.ac.za>
2018-09-11 14:05:42 +0200

    Massively optimised grouping, saving broken

--- dashboard/apps/dashboard_api/management/commands/import_data.py
+++ dashboard/apps/dashboard_api/management/commands/import_data.py
@@ -1,15 +1,17 @@
 from pprint import pprint
-from typing import Dict, List, Type
+from typing import Dict, List, Type, Tuple
 from django.db import transaction
 import pandas as pd
 from django.db.models import Model
 from django.db.models.fields.related import ForeignKey
 from django.db.models.fields.reverse_related import ForeignObjectRel
+from dashboard.apps.dashboard_api.management.measure import Timer
 from dashboard.apps.dashboard_api.models import StudentInfo, ProgramInfo, CourseStats, AverageYearMarks
 from django.core.management.base import BaseCommand
 import os
 import logging
 from dashboard.apps.dashboard_api.management.data_util import load_table
+import numpy as np
 
 logger = logging.getLogger('debug-import')
 
@@ -22,13 +24,12 @@ class Inserter(object):
         self.pk = self.model._meta.pk
         # get fields
         self.fields = {f.name: f for f in model._meta.get_fields()}
-        self.fields = {k: f for k, f in self.fields.items() if f != 'id' and not isinstance(f, ForeignObjectRel)}
+        self.fields = {k: f for k, f in self.fields.items() if k != 'id' and not isinstance(f, ForeignObjectRel)}
         # get fields keys
         self.fks = {k: f for k, f in self.fields.items() if isinstance(f, ForeignKey)}
         self.pks = {k: f for k, f in self.fields.items()} if self.pk.name == 'id' else {self.pk.name: self.pk}
 
-    def _group(self, df: pd.DataFrame) -> List[Dict]:
-        logger.info(f"[{self.model.__name__}]: Grouping...")
+    def _group_old_old(self, df: pd.DataFrame) -> List[Dict]:
         logger.info(f"[{self.model.__name__}]: Grouping...")
         grouped = df.groupby(list(self.pks.keys()))
         entries = []
@@ -41,8 +42,35 @@ class Inserter(object):
         logger.info(f"[{self.model.__name__}]: {len(entries)} {list(self.pks.keys())}")
         return entries
 
-    @transaction.atomic
-    def _save(self, grouped: List[Dict]):
+    def _group_old(self, df: pd.DataFrame) -> Tuple[List[Dict], List[int]]:
+        logger.info(f"[{self.model.__name__}]: Grouping...")
+        indices = [df.columns.get_loc(c) for c in self.fields.keys()]
+        entries = []
+        grouped = df.groupby(list(self.pks.keys()))
+        for i, (key, dfgroup) in enumerate(grouped):
+            if (i+1) % 10000 == 0 or i+1 == grouped.ngroups:
+                logger.info(f"[{self.model.__name__}]: {i+1} of {grouped.ngroups}")
+            entries.append(dfgroup.values[0][indices])
+        return entries, indices
+
+    def _group(self, df: pd.DataFrame) -> pd.DataFrame:
+        logger.info(f"[{self.model.__name__}]: Grouping...")
+        # get all data
+        table_column_indices = [df.columns.get_loc(c) for c in self.fields.keys()]
+        table = df.values
+        # get primary key data
+        table_pk_column_indices = [df.columns.get_loc(c) for c in self.pks.keys()]
+        table_pk = list(list(row) for row in table[:, table_pk_column_indices])  # inefficient
+        # get indices of unique primary keys
+        pk_unique, table_indices = np.unique(table_pk, axis=0, return_index=True)
+        # get unique data specific to table
+        grouped = table[table_indices][:, table_column_indices]
+        grouped = pd.DataFrame(data=grouped, columns=table_column_indices)
+        # self explanatory
+        logger.info(f"[{self.model.__name__}]: Grouped! Found {len(grouped)} unique entries")
+        return grouped
+
+    def _save_old(self, grouped: List[Dict]):
         logger.info(f"[{self.model.__name__}]: loading Foreign Data {len(self.fks)}")
         fk_objects = {}
         for fk, fk_field in self.fks.items():
@@ -61,11 +89,13 @@ class Inserter(object):
         logger.info(f"[{self.model.__name__}]: Saving records {len(insert)}")
         self.model.objects.bulk_create(insert.values())
 
-    def insert(self, df: pd.DataFrame):
+    def _save(self, grouped: np.array):
+        logger.error("TODO: saving data")
+        return 0
+
+    def insert(self, df: pd.DataFrame) -> int:
         grouped = self._group(df)
-        self._save(grouped)
-
-
+        return self._save(grouped)
 
 class Command(BaseCommand):
     help = 'Imports data from excel_import/excel_files/ into the database'
@@ -83,12 +113,13 @@ class Command(BaseCommand):
         logger.info(f"Imported: {imported} records from {len(options['files'])} files")
 
     def import_table(self, df: pd.DataFrame):
-
         logger.info("Importing")
-        Inserter(ProgramInfo).insert(df)
-        Inserter(StudentInfo).insert(df)
-        Inserter(CourseStats).insert(df)
-        Inserter(AverageYearMarks).insert(df)
+        count = 0
+        count += Inserter(ProgramInfo).insert(df)
+        count += Inserter(StudentInfo).insert(df)
+        count += Inserter(CourseStats).insert(df)
+        count += Inserter(AverageYearMarks).insert(df)
+        return count
 
         # logger.info("-----------------------------------------------------------------------")
         # logger.info("Inserting data from table to  models: ")
---
+++ dashboard/apps/dashboard_api/management/measure.py
@@ -0,0 +1,33 @@
+import time
+
+
+
+class Timer:
+
+    _call_stack = []
+
+    def __init__(self, name, printer=print):
+        assert name and printer
+        self.start = None
+        self.end = None
+        self.name = name
+        self.printer = printer
+
+    def _print(self, *text):
+        name = '/'.join(Timer._call_stack)
+        text = ''.join(str(t) for t in text)
+        self.printer(f"[\033[93m{name}\033[0m] {text}")
+
+    def __enter__(self):
+        Timer._call_stack.append(self.name)
+        self._print("\033[92m", "Started", "\033[0m")
+        self.start = time.time()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.end = time.time()
+        self._print("\033[91m", "Finished", "\033[0m", ": ", self.duration(), "ms\n")
+        Timer._call_stack.pop()
+
+    def duration(self):
+        return round((self.end - self.start) * 1000 * 1000) / 1000
