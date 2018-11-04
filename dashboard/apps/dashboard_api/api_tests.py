2f1e28198ddba79ccf0eae4cf52896e1de5b65d3
Matthew Cockcroft <cockcroft.matthew@gmail.com>
2018-11-04 16:20:18 +0200

    Started writing api tests

--- dashboard/apps/dashboard_api/api_tests.py
+++ dashboard/apps/dashboard_api/api_tests.py
@@ -1,1 +1,23 @@
-from django.test import TestCase
+from django.test import Client
+
+c = Client()
+
+response = c.post('/login/', {'username': 'john', 'password': 'smith'})
+response.status_code
+
+response = c.post('course_stats/query', {'chain': [{'group': {'by': ['calendar_instance_year']}}]})
+response.content
+
+response = c.post('school_info/query', {'chain': [{'group': {'by': ['faculty']}}]})
+response.content
+
+#response = c.post('school_info/query', {'chain': [{'filter': [{'field': 'faculty', 'operator': 'exact',
+#'value': faculties}]}]})
+#response.content
+
+#response = c.post('course_info/query', {'chain': [{'filter': [{'field': 'school', 'operator': 'exact',
+#'value': schools}]}]})
+#response.content
+
+#response = c.post('course_stats/query', {'chain': [{'filter': [{'field': 'faculty', 'operator': 'exact',
+#'value': faculties}], 'group': {'by': ['groupBy'], 'yield': [{'name': 'count', 'via': 'count', 'from': 'groupBy'}]}}]})
