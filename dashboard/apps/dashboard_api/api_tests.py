4315e8e73efde46fbf1dee8f79bb7eb56dab3c82
Matthew Cockcroft <cockcroft.matthew@gmail.com>
2018-11-05 11:57:05 +0200

    Changed tests with updated api queries

--- dashboard/apps/dashboard_api/api_tests.py
+++ dashboard/apps/dashboard_api/api_tests.py
@@ -1,23 +1,29 @@
 from django.test import Client
 
-c = Client()
 
-response = c.post('/login/', {'username': 'john', 'password': 'smith'})
-response.status_code
+def test_api_queries(self):
+    c = Client()
 
-response = c.post('course_stats/query', {'chain': [{'group': {'by': ['calendar_instance_year']}}]})
-response.content
+    response = c.post('/login/', {'username': 'john', 'password': 'smith'})
+    response.status_code
 
-response = c.post('school_info/query', {'chain': [{'group': {'by': ['faculty']}}]})
-response.content
+    response = c.post('/login/', {'username': 'user', 'password': 'pass'})
+    response.status_code
 
-#response = c.post('school_info/query', {'chain': [{'filter': [{'field': 'faculty', 'operator': 'exact',
-#'value': faculties}]}]})
-#response.content
+    #response = c.post('course_stats/query', {'chain': [{'group': {'by': ['calendar_instance_year'], 'distinctGrouping': 'true', 'removeDuplicateCountings': 'false'}}]})
+    #response.content
 
-#response = c.post('course_info/query', {'chain': [{'filter': [{'field': 'school', 'operator': 'exact',
-#'value': schools}]}]})
-#response.content
 
-#response = c.post('course_stats/query', {'chain': [{'filter': [{'field': 'faculty', 'operator': 'exact',
-#'value': faculties}], 'group': {'by': ['groupBy'], 'yield': [{'name': 'count', 'via': 'count', 'from': 'groupBy'}]}}]})
+    #response = c.post('school_info/query', {'chain': [{'group': {'by': ['faculty'], 'distinctGrouping': 'true', 'removeDuplicateCountings': 'false'}}]})
+    #response.content
+
+    #response = c.post('school_info/query', {'chain': [{'filter': [{'field': 'faculty', 'operator': 'exact', 'value': faculties}], 'group': {'by': ['school'], 'distinctGrouping': 'true','removeDuplicateCountings': 'false'}}]})
+    #response.content
+
+    #response = c.post('course_info/query', {'chain': [{'filter': [{'field': 'school', 'operator': 'exact', 'value': schools}], 'group': {'by': ['course_name'], 'distinctGrouping': 'true','removeDuplicateCountings': 'false'}}]})
+    #response.content
+
+    #response = c.post('course_stats/query', {'chain': [{'filter': [{'field': 'calendar_instance_year', 'operator': 'exact', 'value': years}, {'field': 'faculty', 'operator': 'exact', 'value': faculties},
+    #{'field': 'school', 'operator': 'exact', 'value': schools}, {'field': 'course_code', 'operator': 'exact', 'value': courses}],
+    #'group': {'by': ['groupBy'], 'yield': [{'name': 'count', 'via': 'count', 'from': 'groupBy'}], 'distinctGrouping': 'false', 'removeDuplicateCountings': 'duplicate'}}]})
+    #response.content
