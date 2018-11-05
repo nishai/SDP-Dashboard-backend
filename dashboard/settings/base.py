4e42987d6ebfd2d5e16461ec122659628b1e41ac
Nathan Michlo <1386161@students.wits.ac.za>
2018-11-05 16:19:18 +0200

    Timeout for queries - TODO, don't think this is working

--- dashboard/settings/base.py
+++ dashboard/settings/base.py
@@ -195,6 +195,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(DATA_DIR, 'db.sqlite3'),
+        'OPTIONS': {
+            'timeout': 10,  # https://docs.python.org/3.7/library/sqlite3.html#sqlite3.connect
+        }
     }
 }
 
