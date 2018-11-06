15d4b17577f25d205e473508f0cecbd41c852df5
Matthew Cockcroft <cockcroft.matthew@gmail.com>
2018-11-06 10:32:28 +0200

    Finished tests

--- dashboard/apps/dashboard_api/api_tests.py
+++ dashboard/apps/dashboard_api/api_tests.py
@@ -21,43 +21,142 @@ class APIQueryTestCase(TestCase):
                     "group": {
                         "by": [
                             'calendar_instance_year'
-                        ]
+                        ],
+                        "distinctGrouping": 'true',
+                        "removeDuplicateCountings": 'false'
                     }
                 }
             ]
         }
         response = self.client.post(
-            '/course_stats/query',json.dumps(python_dict),content_type="application/json")
-        self.assertEqual(response.status_code,200)
+            '/course_stats/query', json.dumps(python_dict), content_type="application/json")
+        self.assertEqual(response.status_code, 200)
 
+        python_dict = {
+            "chain": [
+                {
+                    "group": {
+                        "by": [
+                            'faculty'
+                        ],
+                        "distinctGrouping": 'true',
+                        "removeDuplicateCountings": 'false'
+                    }
+                }
+            ]
+        }
         response = self.client.post(
-            '/school_info/query',
-            {'chain': [{'group': {'by': ['faculty'], 'distinctGrouping': 'true',
-                                  'removeDuplicateCountings': 'false'}}]})
-        self.assertEqual(response.status_code,415)
+            '/school_info/query', json.dumps(python_dict), content_type="application/json")
+        self.assertEqual(response.status_code, 200)
 
+        python_dict = {
+            "chain": [
+                {
+                    "group": {
+                        "by": [
+                            'school'
+                        ],
+                        "distinctGrouping": 'true',
+                        "removeDuplicateCountings": 'false'
+                    }
+                }
+            ]
+        }
         response = self.client.post(
-            '/school_info/query',
-            {'chain': [{'filter': [{'field': 'faculty', 'operator': 'exact', 'value': 'Science'}],
-                        'group': {'by': ['school'], 'distinctGrouping': 'true',
-                                  'removeDuplicateCountings': 'false'}}]})
-        self.assertEqual(response.status_code,415)
+            '/school_info/query', json.dumps(python_dict), content_type="application/json")
+        self.assertEqual(response.status_code, 200)
 
+        python_dict = {
+            "chain": [
+                {
+                    "group": {
+                        "by": [
+                            'course_name'
+                        ],
+                        "distinctGrouping": 'true',
+                        "removeDuplicateCountings": 'false'
+                    }
+                }
+            ]
+        }
         response = self.client.post(
-            '/course_info/query',
-            {'chain': [{'filter': [{'field': 'school', 'operator': 'exact',
-                                    'value': 'Computer Science and Applied Mathematics'}],
-                        'group': {'by': ['course_name'], 'distinctGrouping': 'true',
-                                  'removeDuplicateCountings': 'false'}}]})
-        self.assertEqual(response.status_code,415)
+            '/course_stats/query', json.dumps(python_dict), content_type="application/json")
+        self.assertEqual(response.status_code, 200)
 
+        python_dict = {
+            "chain": [
+                {
+                    "filter": [{
+                        "field": 'faculty', "operator": 'exact', "value": 'Science'
+                    }],
+                    "group": {
+                        "by": [
+                            'school'
+                        ],
+                        "distinctGrouping": 'true',
+                        "removeDuplicateCountings": 'false'
+                    }
+                }
+            ]
+        }
         response = self.client.post(
-            '/course_stats/query',
-            {'chain': [{'filter': [{'field': 'calendar_instance_year', 'operator': 'exact', 'value': '2015'},
-                                   {'field': 'faculty', 'operator': 'exact', 'value': 'Science'},
-                                   {'field': 'school', 'operator': 'exact',
-                                    'value': 'Computer Science and Applied Mathematics'},
-                                   {'field': 'course_code', 'operator': 'exact', 'value': 'COMS1018'}],
-                        'group': {'by': ['gender'], 'yield': [{'name': 'count', 'via': 'count', 'from': 'gender'}],
-                                  'distinctGrouping': 'false', 'removeDuplicateCountings': 'duplicate'}}]})
-        self.assertEqual(response.status_code,415)
+            '/school_info/query', json.dumps(python_dict), content_type="application/json"
+            )
+        self.assertEqual(response.status_code, 200)
+
+        python_dict = {
+            "chain": [
+                {
+                    "filter": [{
+                        "field": 'school', "operator": 'exact', "value": 'Computer Science and Applied Mathematics'
+                    }],
+                    "group": {
+                        "by": [
+                            'course_name'
+                        ],
+                        "distinctGrouping": 'true',
+                        "removeDuplicateCountings": 'false'
+                    }
+                }
+            ]
+        }
+        response = self.client.post(
+            '/course_info/query', json.dumps(python_dict), content_type="application/json"
+        )
+        self.assertEqual(response.status_code, 200)
+
+        python_dict = {
+            "chain": [
+                {
+                    "filter": [{
+                            "field": 'calendar_instance_year', "operator": 'exact', "value": '2015'
+                        },
+                        {
+                            "field": 'faculty', "operator": 'exact', "value": 'Science'
+                        },
+                        {
+                            "field": 'school', "operator": 'exact', "value": 'Computer Science and Applied Mathematics'
+                        },
+                        {
+                            "field": 'course_code', "operator": 'exact', "value": 'COMS1018'
+                        }
+                    ],
+                    "group": {
+                        "by": [
+                            'course_name'
+                        ],
+                        "yield": [
+                            {
+                                "name": 'count', "via": 'count', "from": 'gender'
+                            }
+                        ],
+                        "distinctGrouping": 'true',
+                        "removeDuplicateCountings": 'false'
+                    }
+                }
+            ]
+        }
+        response = self.client.post(
+            '/course_stats/query', json.dumps(python_dict), content_type="application/json"
+        )
+        self.assertEqual(response.status_code, 200)
